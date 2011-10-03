cf46a2e3ac18b47d946d79493c5527d48de9bc89
Daniel Tull <dt@danieltull.co.uk>
2011-10-03 22:25:44 +0100

    Remove DCTStoryboarding

--- DCTStoryboarding.h
+++
@@ -1,9 +0,0 @@
-//
-//  DCTStoryboarding.h
-//  Issues
-//
-//  Created by Daniel Tull on 13.08.2011.
-//  Copyright 2011 Daniel Tull. All rights reserved.
-//
-
-#import "UIViewController+DCTStoryboarding.h"
--- DCTStoryboarding.m
+++
@@ -1,9 +0,0 @@
-//
-//  DCTStoryboarding.m
-//  Issues
-//
-//  Created by Daniel Tull on 13.08.2011.
-//  Copyright 2011 Daniel Tull. All rights reserved.
-//
-
-#import "DCTStoryboarding.h"
